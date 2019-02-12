6515eb6569c1bc259c8a850eca480ee1a7948574
Max Czapanskiy <maxczap@stanford.edu>
2019-02-12 10:49:11 -0800

    Step speeds responds to potentiometer

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-AccelStepper stepper(1, 22, 21);
+/*---------------Constants----------------------------------*/
+#define INPUT_PIN 23
+#define STEP_PIN 22
+#define DIR_PIN 21
+
+#define MAX_SPD 1000
+
+/*---------------Module Variables---------------------------*/
+AccelStepper stepper(1, STEP_PIN, DIR_PIN);
+static uint16_t lastPot;
+static uint16_t currSpd;
 
 /*---------------Teensy Main Functions----------------------*/
 void setup() {
@@ -9,15 +19,25 @@ void setup() {
   Serial.begin(9600);
   while(!Serial);
 
-  // DIR and STEP on pins 22 and 21
-  pinMode(22, OUTPUT);
-  pinMode(21, OUTPUT);
+  // Read input from pin 23
+  pinMode(INPUT_PIN, INPUT);
+
+  // STEP and DIR on pins 22 and 21
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
 
   // Set up motor
-  stepper.setMaxSpeed(1000);
-  stepper.setSpeed(500);
+  stepper.setMaxSpeed(MAX_SPD);
+  digitalWrite(DIR_PIN, HIGH);
 }
 
 void loop() {
+  // Update speed if potentiometer changes
+  uint16_t newPot = analogRead(INPUT_PIN);
+  if (newPot != lastPot) {
+    currSpd = map(newPot, 0, 1023, 0, MAX_SPD);
+    stepper.setSpeed(currSpd);
+    lastPot = newPot;
+  }
   stepper.runSpeed();
 }
